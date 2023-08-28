766a2bb6343b70798456a7e88cdce1c306062fce
Nikolaj <nikolajkc@live.dk>
2023-08-28 08:18:06 +0200

    initial commit

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
         System.out.println("Test");
 
+        metodeTest();
+
     }
 
+
+    public static void metodeTest(){
+        System.out.println("Dette er en metodeTest til branch");
+
+    }
 
 }
