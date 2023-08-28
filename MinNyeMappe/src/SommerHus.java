279dd6bfb7197ac61e0f192f6a1414798860d2a2
Nikolaj <nikolajkc@live.dk>
2023-08-28 09:50:55 +0200

    initial commit

--- MinNyeMappe/src/SommerHus.java
+++ MinNyeMappe/src/SommerHus.java
@@ -1,8 +1,8 @@
 public class SommerHus extends Hus{
     private String gamlePersonNavn;
 
-    public SommerHus(String adresse, int nr, String husNr,int kvadratMeter,String gamlePersonNavn){
-        super(adresse,nr,husNr,kvadratMeter);
+    public SommerHus(String adresse, int nr, int kvaGrund, String husNr,int kvadratMeter,String gamlePersonNavn){
+        super(adresse,nr,kvaGrund,husNr,kvadratMeter);
     }
 
 }
