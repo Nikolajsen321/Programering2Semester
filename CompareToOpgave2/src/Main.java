bbcb806ed57db23519c9c1d499cf4d6ceb8ad1c8
Nikolaj <nikolajkc@live.dk>
2023-09-07 08:40:42 +0200

    initial commit

---
+++ CompareToOpgave2/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Customer [] listCustomer = new Customer[3];
+
+        Customer c1 = new Customer("Mikkel","Bang",45);
+        Customer c2 = new Customer("Niels","Bang",20);
+        Customer c3 = new Customer("Loshan","Bang",300);
+
+        listCustomer[0] = c1;
+        listCustomer[1] = c2;
+        listCustomer[2] = c3;
+
+        System.out.println(lastCustomer(listCustomer).toString());
+        System.out.println();
+
+        System.out.println(Arrays.toString( afterCustomer(listCustomer,c3)));
+
+
+    }
+
+    private static Customer[] afterCustomer(Customer[] listCustomer, Customer customer) {
+        Customer[] customers = null;
+        Customer temp = customer;
+        int størreEnd = 0;
+        for(int i = 0; i < listCustomer.length; i++){
+            if(listCustomer[i].compareTo(customer) > 0){
+                størreEnd++;
+            }
+        }
+        customers = new Customer[størreEnd];
+        for(int i = 0; i < customers.length; i++){
+            if(listCustomer[i].compareTo(customer) > 0){
+                customers[i] = listCustomer[i];
+                System.out.println(listCustomer[i].compareTo(customer));
+            }
+        }
+//        System.out.println(størreEnd);
+        return customers;
+    }
+
+    private static Customer lastCustomer(Customer[] listCustomer) {
+        Customer customer = listCustomer[0];
+        for(Customer c: listCustomer){
+            if(c.compareTo(customer)> 0){
+                customer = c;
+            }
+        }
+        return customer;
+
+    }
+}
